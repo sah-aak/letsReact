0ad36b1e78937a1c2a2d10c470a98912bf3d40c0
Aakriti Sahrawat <sahrawataakriti2@gmail.com>
2020-10-08 16:58:59 +0530

    added links to routes

---
+++ src/CommonComponents/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+class Footer extends React.Component{
+    render(){
+        return (
+            <div className="footer">
+                <p>created with &#65039; by <a href="https://www.linkedin.com/in/aakriti-sahrawat"> </a></p>
+            </div>
+        );
+    }
+}
--- src/CommonComponents/Sidebar/Sidebar.jsx
+++ src/CommonComponents/Sidebar/Sidebar.jsx
@@ -1,22 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Sidebar extends React.Component{
     render(){
         return (
             <div className="sideNav">
+                <Link to='/'>
                     <div className="side-nav-button center-text">Home</div>
+                </Link>
                     <div className="side-nav-button center-text">Live Code</div>
+                <Link to='/livecode'>
                     <div className="side-nav-button center-text">Component Demo</div>
+                </Link>
+                    
+                <Link to='/functionalComponents'>
                     <div className="side-nav-button center-text">Functional Component</div>
+                </Link>
+                <Link to='/propsAndState'>
                     <div className="side-nav-button center-text">Props & State</div>
+                </Link>
+                <Link to='/lifeCycle'>
                     <div className="side-nav-button center-text">React Life Cycle Method</div>
+                </Link>
+                <Link to='/hooks'>
                     <div className="side-nav-button center-text">React Hooks</div>
+                </Link>
+                <Link to='/formHandling'>
                     <div className="side-nav-button center-text">Form Handling</div>
+                </Link>
+                <Link to='/todolist'>
                     <div className="side-nav-button center-text">To Do List App</div>
+                </Link>
+                <Link to='/redux'>
                     <div className="side-nav-button center-text">Redux & Reducers</div>
+                </Link>
+                <Link to=''>
                     <div className="side-nav-button center-text">Fetching Data</div>
+                </Link>
+                <Link to='/weather'>
                     <div className="side-nav-button center-text">Weather App</div>
+                </Link>
+                <Link to='/contactus'>
                     <div className="side-nav-button center-text">Contact Us</div>
+                </Link>   
             </div>
         
         );
